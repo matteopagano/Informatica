b1737a56060241ddd904878840972363435ddf13
Matteo Pagano <metiupaga8@gmail.com>
2017-11-29 18:34:57 +0100

    Update Simul2.java

--- RisorseCondivise/simul2/Simul2.java
+++ RisorseCondivise/simul2/Simul2.java
@@ -9,12 +9,13 @@ import static simul2.Risorsa.semaphore;
 public class Simul2 {
 
     public static void main(String[] args) {
-        System.out.println("Total available Semaphore permits : " + semaphore.availablePermits());
+        System.out.println("Risorse disponibili di entrambi i thread assieme adesso: " + semaphore.availablePermits());
         Task t2 = new Task("B");
         Task t1 = new Task("A");
         
         t1.start();
         t2.start();
+        System.out.println(" : Risorse disponibili di entrambi i thread assieme adesso: " + semaphore.availablePermits());
     }
 
 }
