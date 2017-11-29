8ae94d3024d256dd61fea37b50ae796d02b44e88
Matteo Pagano <metiupaga8@gmail.com>
2017-11-29 18:35:48 +0100

    Update Simul1.java

--- RisorseCondivise/simul1/Simul1.java
+++ RisorseCondivise/simul1/Simul1.java
@@ -7,15 +7,13 @@ import static simul1.Risorsa.semaphore;
  * @author matteo
  */
 public class Simul1 {
-    public static void main(String[] args) {
-
-        System.out.println("Total available Semaphore permits : " + semaphore.availablePermits());
-
-        Task t1 = new Task("A");
-        t1.start();
-
+public static void main(String[] args) {
+        System.out.println("Risorse disponibili di entrambi i thread assieme adesso: " + semaphore.availablePermits());
         Task t2 = new Task("B");
+        Task t1 = new Task("A");
+        
+        t1.start();
         t2.start();
-
+        System.out.println(" : Risorse disponibili di entrambi i thread assieme adesso: " + semaphore.availablePermits());
     }
 }
